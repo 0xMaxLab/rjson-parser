6a172088b26e148fbff7136c4912d00a613c479a
0xMaxLab <minam070902@gmail.com>
2026-03-26 21:25:36 +0100

    updated keepKeyCommand imports

--- src/js/main/RichJson_keepKeyCommands.js
+++ src/js/main/RichJson_keepKeyCommands.js
@@ -1,5 +1,5 @@
-import {cloneObject} from "./RichJsonHelper";
-import {__RICH_JSON_KEY_COMMAND_MEMBER} from "./RichJson";
+import {cloneObject} from "./RichJsonHelper.js";
+import {__RICH_JSON_KEY_COMMAND_MEMBER} from "./RichJson.js";
 
 /**
  * Make key commands constant for given JSON object.
